21c0907a55db72e53688e7ff4cf752de5b03c4ae
Andrei Ladyka <andrei@ladyka.by>
2026-04-22 22:49:16 +0300

    @EnableAsync

--- src/main/java/by/ladyka/poputka/PoputkaApiApplication.java
+++ src/main/java/by/ladyka/poputka/PoputkaApiApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableJpaRepositories
 @EnableScheduling
+@EnableAsync(proxyTargetClass = true)
 class PoputkaApiApplication {
 
     public static void main(String[] args) {
